e039d93669c815cd9df5dbc1997d926a3a529edc
Tomohiro Ubukata <ykmr729@gmail.com>
2018-05-12 22:21:32 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,6 @@ DOT_DIRECTORY="${HOME}/dotfiles"
 cd ${DOT_DIRECTORY}
 for f in .??*
 do
-  [ "$f" = ".DS_Store" ] && continue
-  [ "$f" = ".Trash" ] && continue
-  [ "$f" = ".bash_history" ] && continue
-  [ "$f" = ".histfile" ] && continue
-  [ "$f" = ".python_history" ] && continue
+  [ "$f" = ".git" ] && continue
   ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
 done

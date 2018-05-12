ee39ccf5cf1fae3387a7cf795c4d01efcc4cb338
Tomohiro Ubukata <ykmr729@gmail.com>
2018-05-12 21:43:27 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -u
+DOT_DIRECTORY="${HOME}/dotfiles"
+
+cd ${DOT_DIRECTORY}
+for f in .??*
+do
+  [ "$f" = ".git" ] && continue
+  ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
+done

4dc095032f3e3e12df77722d1002cdfa319fad4e
Tomohiro Ubukata <ykmr729@gmail.com>
2018-05-12 21:48:43 +0900

    add files to ignore

--- install.sh
+++ install.sh
@@ -2,10 +2,13 @@
 
 set -u
 DOT_DIRECTORY="${HOME}/dotfiles"
-
 cd ${DOT_DIRECTORY}
 for f in .??*
 do
-  [ "$f" = ".git" ] && continue
+  [ "$f" = ".DS_Store" ] && continue
+  [ "$f" = ".Trash" ] && continue
+  [ "$f" = ".bash_history" ] && continue
+  [ "$f" = ".histfile" ] && continue
+  [ "$f" = ".python_history" ] && continue
   ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
 done

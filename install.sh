63395f82503602e7196562102c2eb705192db205
Tomohiro Ubukata <ykmr729@gmail.com>
2019-05-17 20:06:26 +0900

    Fix install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 set -u
-DOT_DIRECTORY="${HOME}/dotfiles"
-cd ${DOT_DIRECTORY}
-for f in .??*
-do
-  [ "$f" = ".git" ] && continue
-  ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
+here=$(cd $(dirname $0); pwd)
+for f in .??*; do
+  [[ "$f" = .git* ]] && continue
+  ln -snfv ${here}/${f} ~/${f}
 done

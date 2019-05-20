308b2d230da9cc6c243b2e29627f409762e3374e
Tomohiro Ubukata <ykmr729@gmail.com>
2019-05-20 19:38:53 +0900

    Fix install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 set -u
-here=$(cd $(dirname $0); pwd)
+here=$(cd "$(dirname "$0")" || exit; pwd)
 for f in .??*; do
   [[ "$f" = .git* ]] && continue
-  ln -snfv ${here}/${f} ~/${f}
+  ln -snfv "${here}"/"${f}" ~/"${f}"
 done

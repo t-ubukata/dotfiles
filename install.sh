62d18c2027c9a156061361137d26b848e3578d19
Tomohiro Ubukata <ykmr729@gmail.com>
2021-06-07 00:37:58 +0900

    Fix install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,13 @@ set -u
 here=$(cd "$(dirname "$0")" || exit; pwd)
 for f in .??*; do
   [[ "$f" = .git* ]] && continue
-  ln -snfvT "$here"/"$f" ~/"$f"
+  case "$OSTYPE" in
+  darwin*)
+    ln -snfvF "$here"/"$f" ~/"$f"
+    ;;
+  linux*)
+    ln -snfvT "$here"/"$f" ~/"$f"
+    ;;
+  esac
 done
+
